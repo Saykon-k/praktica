956b2ca0a688e46046f71b83ff203ae572ae98e4
Konstantin <saykovkon@gmail.com>
2020-07-15 02:46:40 +0300

    press f.

--- 14_07_20_pracktica/src/dz1_rasp.java
+++ 14_07_20_pracktica/src/dz1_rasp.java
@@ -7,14 +7,74 @@ import java.util.Random;
 public class dz1_rasp {
 
 	public static void main(String[] args) {
-		HashMap<String , ArrayList<String>> some =states(4,4);
+		HashMap<String , ArrayList<String>> some =states(3,3);
 	    ArrayList<String> r = new ArrayList<String>();
 	    Map<String,String> match = new  HashMap<String,String>(); 
-	    match.put("m1","w2");
-	    match.put("m2","w3");
-	    match.put("m3","w1");
-	    match.put("m4","w4");
+	    some.clear();
+	    r.add("w4");
+	    r.add("w3");
+	    r.add("w1");
+	    r.add("w2");
+	    some.put("m1", (ArrayList<String>) r.clone());
+	    r.clear();
+	    
+	    r.add("w4");
+	    r.add("w2");
+	    r.add("w3");
+	    r.add("w1");
+	    some.put("m2", (ArrayList<String>) r.clone());
+	    r.clear();
+	  
+	    r.add("w4");
+	    r.add("w3");
+	    r.add("w1");
+	    r.add("w2");
+	    some.put("m3", (ArrayList<String>) r.clone());
+	    r.clear();
+	    
+	    r.add("w2");
+	    r.add("w4");
+	    r.add("w3");
+	    r.add("w1");
+	    some.put("m4", (ArrayList<String>) r.clone());
+	    r.clear();
+	    
+	    r.add("m4");
+	    r.add("m2");
+	    r.add("m3");
+	    r.add("m1");
+	    some.put("w1", (ArrayList<String>) r.clone());
+	    r.clear();
+	    
+	    
+	    r.add("m4");
+	    r.add("m3");
+	    r.add("m1");
+	    r.add("m2");
+	    some.put("w2", (ArrayList<String>) r.clone());
+	    r.clear();
+	    
+	    
+	    r.add("m2");
+	    r.add("m1");
+	    r.add("m4");
+	    r.add("m3");
+	    some.put("w3", (ArrayList<String>) r.clone());
+	    r.clear();	    
+	    
+	    r.add("m3");
+	    r.add("m2");
+	    r.add("m1");
+	    r.add("m4");
+	    some.put("w4", (ArrayList<String>) r.clone());
+	    r.clear();
+	    
+	    match.put("m1", "w1");
+	    match.put("m2", "w2");
+	    match.put("m3", "w3");
+	    match.put("m4", "w4");
 
+//
 	    for (Map.Entry entry : some.entrySet()) {
 	        System.out.println("Key: " + entry.getKey() + " Value: "
 	            + entry.getValue());
@@ -23,9 +83,9 @@ public class dz1_rasp {
 	        System.out.println("Key: " + entry.getKey() + " Value: "
 	            + entry.getValue());
 	    }
-	    blockpar(some,match);
-	    goodstate(some,match);
-	   
+	    //blockpar(some,match);
+	    System.out.println(goodstate(some,match).toString());
+	    //goodstate(some,match);
 	}
 	//генерация изначальных данных 
 	public static HashMap<String,ArrayList<String>> states(int man,int woman){
@@ -67,8 +127,8 @@ public class dz1_rasp {
 					//здесь говорится достанем значение из списка два( не ключ) сформируем его список предпочтений и найдем в нем кандидата остальные две проверки написаны по соображениям выше 
 					if(allpriority.get(w.getValue()).indexOf(m)<allpriority.get(w.getValue()).indexOf(w.getKey()) && allpriority.get(w.getValue()).indexOf(m)>-1 && allpriority.get(w.getValue()).indexOf(w.getKey())>-1) {
 						//здесь пара  1 2 более предпочтительная,чем 1-3  
-						System.out.println(m+" "+ w.getValue()+" "+allpriority.get(m).get(state) );
-						String s[]= { m, (String) w.getValue() ,allpriority.get(m).get(state) };
+						//System.out.println(m+" "+ w.getValue()+" "+allpriority.get(m).get(state) );
+						String s[]= { m, (String) w.getValue()};
 						block.add(s);
 					}
 					
@@ -77,18 +137,35 @@ public class dz1_rasp {
 	}
 		return block;
 }
+	public static  Map<String,String> breakpar( Map<String,String> match,String pair[]){
+		 Map<String,String> par = new  HashMap<String,String>() ;
+		 String s = match.get(pair[0]);
+		 String s1 = "";
+		for(Map.Entry w : match.entrySet()) {
+			if(w.getValue().equals(pair[1])) {
+				s1 = (String)w.getKey();
+				//match.remove(w.getKey());
+			}
+		}
+		 match.remove(s1);
+		 match.remove(pair[0]);
+		 match.put(pair[0], pair[1]);
+		 match.put(s1, s);
+		 par = match;
+		return par;
+	}
+	
 	public static Map<String,String>  goodstate(HashMap<String , ArrayList<String>> allpriority,  Map<String,String> match ){
-					Map<String,String> goodst =new HashMap<String , String>();
+					Map<String,String> goodst = match;
 					ArrayList<String[]> check = new ArrayList<String[]>();
+					check = blockpar(allpriority,goodst);
 					while(!check.isEmpty()) {
-						check = blockpar(allpriority,match);
-						if(!check.isEmpty()) {
-							String rep []= check.get(0);
-							match.replace(rep[0], rep[2], rep[1]);
-							//match.re
-						}
+						//System.out.println(goodst.toString());
+						System.out.println(check.get(0)[0] + " " + check.get(0)[1]);
+
+						goodst = breakpar(goodst,check.get(0));
+						check = blockpar(allpriority,goodst);
 					}
-					goodst = match;
 				return goodst;
 
 		
