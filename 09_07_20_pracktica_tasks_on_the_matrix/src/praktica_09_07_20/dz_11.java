26ceb31850644760409e86faed3a8122ed7b9db8
Konstantin <saykovkon@gmail.com>
2020-07-27 19:25:23 +0300

    rename

---
+++ 09_07_20_pracktica_tasks_on_the_matrix/src/praktica_09_07_20/dz_11.java
@@ -0,0 +1,140 @@
+package praktica_09_07_20;
+
+public class dz_11 {
+
+	public static void main(String[] args) {
+		int matr[][] = {{1,1,0},{1,1,0},{1,1,0}};
+		System.out.println(gorizont(matr));
+		System.out.println(vertical(matr));
+		System.out.println(diagonal(matr));
+
+		printmatrix(matr);
+	}
+	public static int gorizont(int matr[][]) {
+		//вводим две перменные промежуточную и "настоящую
+		int len = -1;
+		int pobochlen = 0;
+		for(int i = 0; i<matr.length;i++) {
+			for(int j = 0; j<matr[0].length;j++) {
+				if(matr[i][j]==0) {
+					pobochlen++;
+				}else {
+					if(pobochlen>len) {
+						len = pobochlen;
+					}
+					pobochlen=0;
+				}
+			}
+			//обязательная проверка, так как может быть так, что есть одна полностью заполненная строчка
+			//и на след строчке ноль, поэтому обнуление и проверка
+			if(pobochlen>len) {
+				len = pobochlen;
+			}
+			pobochlen=0;
+		}
+		return len;
+	}
+	public static int vertical(int matr[][]) {
+		int len = -1;
+		int pobochlen = 0;
+		for(int i = 0; i<matr.length;i++) {
+			for(int j = 0; j<matr[0].length;j++) {
+				if(matr[j][i]==0) {
+					pobochlen++;
+				}else {
+					if(pobochlen>len) {
+						len = pobochlen;
+					}
+					pobochlen=0;
+				}
+			}
+			if(pobochlen>len) {
+				len = pobochlen;
+			}
+			pobochlen=0;
+		}
+		return len;
+	}
+	public static int diagonal(int matr[][]) {
+		int len = -1;
+		int pobochlen = 0;
+		//нижняя диагональ главная
+		for(int j = 0; j<matr.length;j++ ) {
+		for(int i = 0; i < matr.length-j;i++) {
+			if(matr[i+j][i]==0) {
+				pobochlen++;
+			}else {
+				if(pobochlen>len) {
+					len = pobochlen;
+				}
+				pobochlen=0;
+			}
+		}
+		if(pobochlen>len) {
+			len = pobochlen;
+		}
+		pobochlen=0;
+		}
+		//верхняя диагональ главня 
+		for(int j = 0; j<matr.length;j++ ) {
+			for(int i = 0; i < matr.length-j;i++) {
+				if(matr[i][i+j]==0) {
+					pobochlen++;
+				}else {
+					if(pobochlen>len) {
+						len = pobochlen;
+					}
+					pobochlen=0;
+				}
+			}
+			if(pobochlen>len) {
+				len = pobochlen;
+			}
+			pobochlen=0;
+			}
+		//побочная дигональ снизу обход
+		for(int j = 0; j< matr.length;j++) {
+		for(int i =  matr.length-1; i > -1+j;i--) {
+			if(matr[i][matr.length-1-i+j]==0) {
+				pobochlen++;
+			}else {
+				if(pobochlen>len) {
+					len = pobochlen;
+				}
+				pobochlen=0;
+			}
+		}
+		if(pobochlen>len) {
+			len = pobochlen;
+		}
+		pobochlen=0;
+		}
+		//побочная диагональ сверху обход
+		for(int j = 0; j < matr.length;j++) {
+		for(int i =  matr.length-1; i > -1+j;i--) {
+			if(matr[i-j][matr.length-1-i]==0) {
+				pobochlen++;
+			}else {
+				if(pobochlen>len) {
+					len = pobochlen;
+				}
+				pobochlen=0;
+			}
+		}
+		if(pobochlen>len) {
+			len = pobochlen;
+		}
+		pobochlen=0;
+		}
+
+		return len;
+	}
+	public static void printmatrix(int matr[][]) {
+		for(int i = 0; i < matr.length;i++) {
+			for(int j = 0; j < matr.length;j++) {
+				System.out.print(matr[i][j]+" ");
+			}
+			System.out.println();
+		}
+	}
+}
